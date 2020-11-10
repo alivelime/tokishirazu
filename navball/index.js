ced17cafd7ca01147aef0c2e5974af6159f672dd
eto-shingo <=>
2020-11-10 11:09:39 +0900

    navball fix やっぱり方位90°ずれてる。。

--- navball/index.js
+++ navball/index.js
@@ -71,7 +71,7 @@ function setDeviceOrientationEvent(camera) {
   window.addEventListener('deviceorientation', e => {
     e.preventDefault()
     camera.rotation.x = THREE.MathUtils.degToRad(- e.beta) // 仰角・俯角
-    camera.rotation.y = THREE.MathUtils.degToRad(e.alpha + 180) // 方位
+    camera.rotation.y = THREE.MathUtils.degToRad(e.alpha + 90) // 方位
     camera.rotation.z = THREE.MathUtils.degToRad(- e.gamma)  // ねじれ
   })
 }
