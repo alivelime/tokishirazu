090336b893f0ab5d398fd02b39bc212567a8d3a4
eto-shingo <=>
2020-11-30 12:10:08 +0900

    vanball lotate 90 2

--- navball/index.js
+++ navball/index.js
@@ -70,9 +70,9 @@ function setDeviceOrientationEvent(camera) {
   // 許可を得られた場合、deviceorientationをイベントリスナーに追加
   window.addEventListener('deviceorientation', e => {
     e.preventDefault()
-    camera.rotation.x = THREE.MathUtils.degToRad(- e.beta - 90) // 仰角・俯角
-    camera.rotation.y = THREE.MathUtils.degToRad(e.alpha + 180) // 方位
-    camera.rotation.z = THREE.MathUtils.degToRad(- e.gamma)  // ねじれ
+    camera.rotation.x = THREE.MathUtils.degToRad(e.beta) // 仰角・俯角
+    camera.rotation.y = THREE.MathUtils.degToRad(e.alpha) // 方位
+    camera.rotation.z = THREE.MathUtils.degToRad(e.gamma)  // ねじれ
   })
 }
 
